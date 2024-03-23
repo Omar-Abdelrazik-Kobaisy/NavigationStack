f18bb92531fd69d356dfbd244c9fb103217e7e52
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2024-03-23 13:10:16 +0200

    Initial Commit

---
+++ how-to-use-navigationStack#1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  how-to-use-navigationStack#1
+//
+//  Created by Omar on 23/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ how-to-use-navigationStack#1/how_to_use_navigationStack_1App.swift
@@ -0,0 +1,17 @@
+//
+//  how_to_use_navigationStack_1App.swift
+//  how-to-use-navigationStack#1
+//
+//  Created by Omar on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct how_to_use_navigationStack_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
